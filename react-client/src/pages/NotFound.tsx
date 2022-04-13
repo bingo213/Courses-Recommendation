d96587085d470cce120b5f0962165a53e23738d5
Trần Thị Hoa Hiên <56663780+bingo213@users.noreply.github.com>
2022-04-14 00:54:42 +0700

    feat: not found page

--- react-client/src/pages/NotFound.tsx
+++ react-client/src/pages/NotFound.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import notFoundImg from '../assets/not_found.png';
 
 export const NotFound: React.FC<{}> = () => {
-  return <div>NotFound</div>;
+  return (
+    <Wrapper>
+      <img src={notFoundImg} alt="404 not found" />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
